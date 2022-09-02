ccd88406c63aa2fdf4ceb441ed10a6d6bb261603
nimishanb <88263517+nimishanb@users.noreply.github.com>
2022-09-02 14:50:08 +0530

    Update from_excel.py

--- pyscreenscrape/from_excel.py
+++ pyscreenscrape/from_excel.py
@@ -1,10 +1,8 @@
 import openpyxl
 # THINGS TO DO
-# to check if its the right udise code
-# to check if it has the details page
 # if run twice the data gets appended again(even if presesnt before)
 
-wb_1 = openpyxl.load_workbook('topython.xlsx')
+wb_1 = openpyxl.load_workbook('Bilal School UDISE Codes.xlsx')
 ws_excel = wb_1.active
 udise_codes = []
 column_a = ws_excel['A']
